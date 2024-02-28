63de7aca0f89a32f1b301b0000452106972f3b82
Jainish06 <pathakjainish@gmail.com>
2024-02-28 18:42:22 +0530

    Fresh start from previous build afte reinstalling os.

---
+++ Screens/Homepage.jsx
@@ -0,0 +1,113 @@
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView, ImageBackground, useColorScheme} from 'react-native'
+import React from 'react'
+import { useNavigation} from '@react-navigation/native';
+
+export default function Homepage() {
+  const navigation = useNavigation();
+  const colorScheme = useColorScheme() === 'dark'
+  return (
+    <SafeAreaView style={styles.test}>
+      <ImageBackground source={require('./peakpx.jpg')} style={styles.img}>
+        <ScrollView>
+          <Text style={colorScheme ? styles.dark : styles.white}>BrawnBlitz</Text>
+          <Text style={colorScheme ? styles.dark1 : styles.white1}>Your Personel Trainer</Text>
+          <View style={styles.container}>
+            <View style={styles.card}>
+              <TouchableOpacity  
+                onPress={() => navigation.navigate('Loginpage')}
+                style={styles.button}>
+                <Text style={styles.text}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity  
+                onPress={() => navigation.navigate('Signuppage')}
+                style={styles.button}>
+                <Text style={styles.text}>Sign-Up</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  test:{
+    flex:1,
+  },
+  white:{
+    color:'white',
+    fontSize:25,
+    fontWeight: 'bold',
+    fontFamily:'TimesNewRoman',
+    letterSpacing: 10,
+    textAlign: 'center',
+    marginTop: 3,
+  },
+  dark:{
+    color:'white',
+    fontSize:25,
+    fontWeight: 'bold',
+    fontFamily:'TimesNewRoman',
+    letterSpacing: 10,
+    textAlign: 'center',
+    marginTop: 3,
+  },
+  white1:{
+    color:'white',
+    fontSize:15,
+    fontWeight: 'bold',
+    fontFamily:'TimesNewRoman',
+    letterSpacing: 2,
+    textAlign: 'center',
+    marginTop: 3,
+  },
+  dark1:{
+    color:'white',
+    fontSize:15,
+    fontWeight: 'bold',
+    fontFamily:'TimesNewRoman',
+    letterSpacing: 2,
+    textAlign: 'center',
+    marginTop: 3,
+  },
+  img:{
+    width:'100%',
+    height:'100%',
+    position:'absolute'
+  },
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+    marginTop: 470,
+  },
+  card:{
+    flex: 1,
+    flexDirection :'row',
+    width:200,
+    height:200,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 5,
+    padding:5,
+    borderRadius: 15,
+    opacity:10
+  },
+  button:{
+    backgroundColor: 'white',
+    width: '45%',
+    paddingVertical: 7,
+    borderRadius: 20,
+    elevation: 12,
+    shadowColor: '#2563eb',
+    marginLeft:10
+  },
+  text:{
+    textAlign: 'center',
+    color: 'black',
+    fontWeight:'bold',
+    fontSize: 20,
+    fontFamily: 'Montserrat-SemiBold',
+  }
+})
---
+++ Screens/Loginpage.jsx
@@ -0,0 +1,128 @@
+import { StyleSheet, Text, View, SafeAreaView, loading, ToastAndroid, Keyboard, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native'
+import React,{useState} from 'react'
+import { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+const Loginpage = ({navigation}) => {
+
+  GoogleSignin.configure({
+    webClientId: '1083046700457-4534l36v1i1omvfoad2cq73ctqota3vg.apps.googleusercontent.com',
+  });
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  const [value, setValue] = useState({
+    email: '',
+    mobile: '',
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {!loading && (
+        <>
+          <View style={styles.inputCont}>
+            <Text style={styles.labelText}>Phone No.</Text>
+            <TextInput
+              value={value.mobile}
+              onChangeText={text => setValue({...value, mobile: text})}
+              style={styles.input}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.btnCont}
+            activeOpacity={0.4}>
+            <Text style={styles.btnText}>Create Account</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btnCont}
+            onPress ={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}>
+            <Text style={styles.btnText}>Continue with Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.4}
+            onPress={() => navigation.navigate('Signuppage')}>
+            <Text style={styles.alreadyText}>Don't Have An Account?</Text>
+          </TouchableOpacity>
+        </>
+      )}
+      {loading && (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator
+            size="large"
+            color="#2563eb"
+            style={{paddingHorizontal: 10, paddingVertical: 6}}
+          />
+        </View>
+      )}
+    </SafeAreaView>
+  )
+}
+
+export default Loginpage
+
+const styles = StyleSheet.create({
+  text:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignContent:'center'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputCont: {
+    width: '85%',
+    marginBottom: 10,
+  },
+  input: {
+    fontSize: 16,
+    borderColor: '#9ca3af',
+    borderWidth: 1.4,
+    backgroundColor: '#F3F4F6',
+    borderRadius: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginBottom: 10,
+    color: '#000',
+    fontFamily: 'Montserrat-Medium',
+  },
+  labelText: {
+    marginBottom: 4,
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  btnCont: {
+    backgroundColor: '#2563eb',
+    width: '85%',
+    paddingVertical: 12,
+    borderRadius: 8,
+    elevation: 14,
+    shadowColor: '#2563eb',
+    marginBottom:10
+  },
+  btnText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontSize: 18,
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  alreadyText: {
+    marginTop: 20,
+    color: '#000',
+    fontSize: 15,
+    fontFamily: 'Montserrat-SemiBold',
+  },
+})
